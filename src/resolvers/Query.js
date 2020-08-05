3ef19e1208714bc32b7e533d9309a7a7b14f2b0e
Francis <francispham89@gmail.com>
2020-08-04 17:55:55 -0700

    Prisma Yoga Flow in Items Creation: Implemented GraphQL Query for Getting Item Data

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,15 @@
-const Query = {};
+// const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  /* 
+  * Using 'forwardTo' when the Query is exactly the same in both
+  * Prisma & Public Query, or no Need for Authentication or Filter
+  items: forwardTo('db'),
+  */ 
+  async items(parent, args, context, info) {
+    const items = await context.db.query.items();
+    return items;
+  }
+};
 
 module.exports = Query;
