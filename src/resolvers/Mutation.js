9444109a5366229483dc953adccfd05b91a92c37
Francis <francispham89@gmail.com>
2020-08-04 17:21:34 -0700

    Prisma Yoga Flow in Items Creation: Implemented GraphQL Mutation for Creating Item Data

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,19 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, context, info) {
+    // TODO: Check if the user login
+
+
+    const item = await context.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info); 
+    // 'info' is required to get back the 'actual query' from
+    // the frontend so 'context' can pass it into the backend
+    // => Specify what data gets returned from the db when we create it.
+    console.log('item:', item)
+    return item;
+  }
+};
 
 module.exports = Mutations;
