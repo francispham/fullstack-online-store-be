e76237d2278a94197d6b6bc1de6787fef303026a
Francis <francispham89@gmail.com>
2020-08-08 16:37:14 -0700

    Prisma Yoga Flow in Items Creation: Implemented GraphQL Query & Mutation for Deleting Items

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -31,6 +31,16 @@ const Mutations = {
       info
     );
   },
+
+  async deleteItem(parent, args, context, info) {
+    const where = { id: args.id };
+    // 1. Find the Item
+    const item = await context.db.query.item({ where }, `{ id title }`);
+    // 2. Check if they own that item, or have the permissions
+    // TODO
+    // 3. Delete it
+    return context.db.mutation.deleteItem({ where }, info);
+  },
 };
 
 module.exports = Mutations;
