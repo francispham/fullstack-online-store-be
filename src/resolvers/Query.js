06857ed8a0f0730ddc01b98815163a31ec3d5463
Francis <francispham89@gmail.com>
2020-08-10 12:08:33 -0700

    Prisma Yoga Flow in Pagination: Database modified & Added skip and first for displaying Items per Page

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,14 +2,14 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
   /* 
-  * Using 'forwardTo' when the Query is exactly the same in both
-  * Prisma & Public Query, or no Need for Authentication or Filter
-  items: forwardTo('db'),
-  */ 
   async items(parent, args, context, info) {
     const items = await context.db.query.items();
     return items;
   },
+  */ 
+  // Using 'forwardTo' when the Query is exactly the same in both
+  // Prisma & Public Query, or no Need for Authentication or Filter
+  items: forwardTo('db'),
   // Query a Single Item
   item: forwardTo('db'),
   // Query for Pagination
